6b4ab5571c33d8c3923b47913997636bc158d9cf
AlexaMUN <alexandrahousing@gmail.com>
2019-11-09 20:07:06 +0100

    Finished week 1 hw

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,65 @@
-// Write your JS here
+var hero = {
+    name: '',
+    inventory: Array(),
+    health: 10,
+    weapon: {
+        type: 'pen',
+        damage: 2,
+    },
+    heroic: false
+};
+
+var dagger = {
+    type: 'dagger',
+    damage: 2
+};
+
+var name = prompt("Please enter your name", "PlayerOne");
+alert("Your name is now " + name);
+
+function rest(person)
+{
+    if (person.health == 10)
+    {
+       window.alert('You\'re already in tip-top shape!');
+        return person;
+    }
+
+    person.health = 10;
+
+    return person;
+
+}
+
+
+
+function pickUpItem(person, weapon)
+{
+    person.inventory.push(weapon);
+
+    return person;
+}
+
+
+function equipWeapon(person)
+{
+    if (person.inventory.length == 0 || person.inventory[0] == person.weapon)
+    {
+        return person;
+    }
+    
+   else person.weapon = person.inventory[0];
+
+    return person;
+}
+// function displayStats(person){
+
+// prompt ("Name " + person.name;
+// var currentHealth = "Your current health level is " + person.health;
+// var currentWeapon = "Your current weapon is " + person.weapon;
+// var currentWeaponDamage = "Your current weapon damage level is " + person.weapon.damage;}
+
+
+// prompt("Your current stats are", displayStats(hero));
+
+window.alert("Name " + hero.name);
